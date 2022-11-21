9b5ad90063a0d977776946ff7d85e7a0f437be11
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-11-21 21:40:12 +0100

    Added stylesheet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import "./App.css";
+import RandomName from "./RandomName";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <RandomName />
     </div>
   );
 }
---
+++ src/RandomName.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function RandomName() {
+  const handleNameChange = () => {
+    const names = [`Bob`, `Matt`, `Dave`];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+  return (
+    <div className="RandomName">
+      <p>Hello {handleNameChange()}!</p>
+    </div>
+  );
+}
