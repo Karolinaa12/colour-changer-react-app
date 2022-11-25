98be05df281f15d273feffccc5cd14f2806ba487
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-11-25 23:47:10 +0100

    Deleted unnessecary things

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Input from "./Input";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <RandomName />
+      <h1>
+        <RandomName />
+      </h1>
       <Square />
       <Input />
     </div>
