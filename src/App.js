a2385cb2aa6afb96dccedbb90fbd09e35d83df7a
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-11-28 20:44:50 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import RandomGreeting from "./RandomGreeting";
 import Square from "./Square";
 import Input from "./Input";
 
 export default function App() {
+  const [color, setColor] = useState(" ");
   return (
     <div className="App">
       <div className="container">
         <h1>
           <RandomGreeting />
         </h1>
-        <Square />
-        <Input />
+        <Square color={color} />
+        <Input color={color} setColor={setColor} />
       </div>
     </div>
   );
